7a3cdf0b1a31aa2c50937a68e8c947e4b724f420
Thomas Espervik <thomas.espervik@kantega.no>
2024-02-01 09:45:53 +0100

    fix: update routes

--- api/routers/guess.py
+++ api/routers/guess.py
@@ -1,15 +1,27 @@
 from fastapi.routing import APIRouter
 from fastapi.responses import JSONResponse
 
-from methods.motd import monsterOfTheDay
-from methods.monsterList import loadMonsterList
+from methods.compare import compareGuess
+from methods.monster import loadSelectedMonsterList, monsterOfTheDay
 
+from interfaces.Guess import Guess
 from interfaces.GameSelection import GameSelection
 
 router = APIRouter()
 
 @router.get("/api/guess")
-async def guess(game_selection: GameSelection):
-    monster_list = loadMonsterList(game_selection)
+async def guess(data: Guess) -> JSONResponse:
+    guess = data.guess
+    game_selection = data.games
+    monster_list = loadSelectedMonsterList(game_selection)
     motd = monsterOfTheDay(monster_list)
-    return JSONResponse(motd)
+    if not (comparison := compareGuess(guess, motd)):
+        return JSONResponse({'status': 404, 'message': 'monster not found'})
+    return comparison
+
+@router.get("/api/cheat")
+async def cheat(game_selection: GameSelection) -> JSONResponse:
+    monster_list = loadSelectedMonsterList(game_selection)
+    motd = monsterOfTheDay(monster_list)
+    print(type(motd))
+    return JSONResponse(motd.__dict__)
