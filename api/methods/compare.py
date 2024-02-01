13c52701b90c1a436bc8b888a425961f0a72644c
Thomas Espervik <thomas.espervik@kantega.no>
2024-02-01 09:38:21 +0100

    wip: guess comparison

--- api/methods/compare.py
+++ api/methods/compare.py
@@ -2,9 +2,11 @@ from interfaces.Games import Games
 from interfaces.Monster import Monster
 from interfaces.Proximity import Proximity
 
+from methods.monster import getMonster
+
 # NOTE removed introduced property
 #  first game in list is game monster was introduced
-def compareGuess(guess: str, motd: Monster) -> Proximity:
+def compareGuess(guess: str, motd: Monster) -> Proximity | None:
     """Compares the user's guess to the monster of the day.
 
     Args:
@@ -15,4 +17,26 @@ def compareGuess(guess: str, motd: Monster) -> Proximity:
         Proximity object with information on how close the guess is.
     """
 
-    return "wrong lmao"
+    if guess == motd.name:
+        return {
+            'correct': 0,
+            'species': 0,
+            'category': 0,
+            'game': 0,
+            'elements': 0,
+            'statuses': 0,
+            'weaknesses': 0
+        }
+
+    if not (monster := getMonster(guess)):
+        return None
+
+    return {
+        'correct': 1,
+        'species': 1,
+        'category': 2,
+        'game': 2,
+        'elements': 2,
+        'statuses': 2,
+        'weaknesses': 2
+    }
