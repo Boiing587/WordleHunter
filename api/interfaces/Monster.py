803349317ff70ebf27a1a5ae7959489b118efbd4
Thomas Espervik <thomas.espervik@kantega.no>
2024-02-03 20:46:52 +0100

    feat: suborder/generation category getter

--- api/interfaces/Monster.py
+++ api/interfaces/Monster.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
 
+from interfaces.Games import Games
+
 class Monster(BaseModel):
     name: str
     type: str
@@ -17,3 +19,15 @@ class Monster(BaseModel):
             "variant": [ "Variant", "Devian" ],
             "apex": [ "Apex", "Zenith" ]
         }
+
+    def suborderCategory(self):
+        suborder_map = self.suborderMapping()
+        reverse_map: dict[str, list] = {v: k for k, values in suborder_map.items() for v in values}
+        category = suborder_map[reverse_map.get(self.suborder)]
+        return category
+
+    def generation(self):
+        generation_map = Games.generationMapping()
+        reverse_map: dict[str, list] = {v: k for k, values in generation_map.items() for v in values}
+        generation = generation_map[reverse_map.get(self.games[0])]
+        return generation
