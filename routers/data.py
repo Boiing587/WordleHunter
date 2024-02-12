32fe0ba82372fc06a602bcd9dfd73741a2e44406
Thomas Espervik <thomas.espervik@kantega.no>
2024-02-12 15:30:56 +0100

    debug: route for checking relative monsters

--- routers/data.py
+++ routers/data.py
@@ -3,8 +3,16 @@ from fastapi.responses import JSONResponse
 
 from interfaces.Games import Games
 
+from methods.monster import loadMonsterList, getMonster, getRelatives
+
 router = APIRouter()
 
 @router.get("/api/data/gamelist")
 async def gameList() -> JSONResponse:
         return JSONResponse(Games.gameNames())
+
+@router.get("/api/data/relatives")
+async def relativeMonsters(name: str) -> JSONResponse:
+        monster_list = loadMonsterList()
+        monster = getMonster(name)
+        return JSONResponse(getRelatives(monster, monster_list))
